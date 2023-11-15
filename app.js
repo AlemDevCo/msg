ad4bdb9998c8a01d45de78d663b91e193f1fe0b0
AlemDevCo <102746202+AlemDevCo@users.noreply.github.com>
2023-11-15 09:39:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const socket = io('https://msg-alem.vercel.app/');
-
 function sendMessage() {
     var messageInput = document.getElementById('messageInput');
     var message = messageInput.value;
+
+    console.log('Sending message:', message);
 
     if (message.trim() !== '') {
         fetch('/api/message', {
@@ -14,6 +14,7 @@ function sendMessage() {
         })
             .then((response) => response.json())
             .then((data) => {
+                console.log('Response:', data);
                 if (data.success) {
                     messageInput.value = '';
                 } else {
@@ -23,17 +24,3 @@ function sendMessage() {
             .catch((error) => console.error('Error:', error));
     }
 }
-
-function displayMessage(message) {
-    var messageContainer = document.getElementById('message-container');
-
-    var newMessage = document.createElement('div');
-    newMessage.className = 'message';
-    newMessage.textContent = message;
-
-    messageContainer.insertBefore(newMessage, messageContainer.firstChild);
-}
-
-socket.on('message', (message) => {
-    displayMessage(message);
-});
